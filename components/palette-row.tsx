8e1908403cbfd75f5fed23e5abf497ac5216eb80
Zoe Hayes <zoehayes286@gmail.com>
2026-02-16 17:58:17 -0500

    Update Readme, fix a comment

--- components/palette-row.tsx
+++ components/palette-row.tsx
@@ -9,17 +9,10 @@ interface IPaletteProps {
 export function PaletteRow(props: IPaletteProps) {
   return (<div className="flex flex-row">
     {props.columns.map((col, i) => {
+      // Color math, generate shades from columns based on lightness and chroma equation
       const l = (1000-col)/1000;
       const base = 0.1;
       const c = base + Math.sin((1-l) * Math.PI) * .1;
-      // const color = `oklch(${l}% ${} ${})`;
-      // console.log(props.hue)
-      // return <span key={i} className="p-4" style={{ 
-      //   backgroundColor: color,
-      //   color: l < 50 ? "black" : "white"
-      //   }}>
-      //   {l * 10}
-      // </span>;
       return <PaletteSwatch l={l} c={c} h={props.hue} key={i}></PaletteSwatch>
     }
     )}
